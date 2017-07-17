6d2b2bde4b164b39122a18b162a978901cd572d9
CoderLiu <liuzexin_top@163.com>
2017-07-17 19:20:04 +0800

    new add test

---
+++ js/exchange.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('li').click(function(){
+		var me = this;
+		$(me).toggleClass('active-li');
+	})
+	$('#sub').click(function(){
+		$('.list-api-left ul').append($('.list-api-right ul .active-li'));		
+	});
+})
